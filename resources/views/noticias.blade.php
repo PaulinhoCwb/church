ea0d4cd0641696e5cad88e57efbe14b74a55e468
João Paulo Miranda <joaopaulocap10@gmail.com>
2019-04-15 07:32:15 -0300

    ajustes css

--- resources/views/noticias.blade.php
+++ resources/views/noticias.blade.php
@@ -3,7 +3,7 @@
 <div class="container-fluid" id="back-news">
     <div class="row">
         <div class="col-md-9 col-xs-12">
-            {{-- @foreach ($noticias as $noticia)
+            @foreach ($noticias as $noticia)
                 <div class="block-news mb-3 mt-2">
                     <h3>{{ $noticia->titulo }}</h3>
     
@@ -15,46 +15,7 @@
                             Mais</a>
                     </div>
                 </div> 
-            @endforeach --}}
-             <div class="block-news mb-3 mt-2">
-                <h3>Titulo da noticia</h3>
-
-                <div class="content-news">
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Enim impedit excepturi, quod
-                        voluptatem quas atque quo obcaecati odit adipisci reiciendis! Reiciendis molestias
-                        quisquam odit in suscipit libero voluptate iure deleniti?</p>
-                </div>
-                <div>
-                    <small>12/03/2019</small> <span>João Paulo</span> <a href="#" class="float-right btn btn-info">Ler
-                        Mais</a>
-                </div>
-            </div>
-            {{-- <div class="block-news mb-3">
-                <h3>Titulo da noticia</h3>
-
-                <div class="content-news">
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Enim impedit excepturi, quod
-                        voluptatem quas atque quo obcaecati odit adipisci reiciendis! Reiciendis molestias
-                        quisquam odit in suscipit libero voluptate iure deleniti?</p>
-                </div>
-                <div>
-                    <small>12/03/2019</small> <span>João Paulo</span> <a href="#" class="float-right btn btn-info">Ler
-                        Mais</a>
-                </div>
-            </div>
-            <div class="block-news mb-3">
-                <h3>Titulo da noticia</h3>
-
-                <div class="content-news">
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Enim impedit excepturi, quod
-                        voluptatem quas atque quo obcaecati odit adipisci reiciendis! Reiciendis molestias
-                        quisquam odit in suscipit libero voluptate iure deleniti?</p>
-                </div>
-                <div>
-                    <small>12/03/2019</small> <span>João Paulo</span> <a href="#" class="float-right btn btn-info">Ler
-                        Mais</a>
-                </div>
-            </div>  --}}
+            @endforeach
         </div>
         <div class="col-md-3 col-xs-12 mt-2">
             <div class="block-intecoes">
